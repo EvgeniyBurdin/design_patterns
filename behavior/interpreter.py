9c2db1ef2c3070f2da0fad65545675d275874683
Evgeniy Burdin <e.s.burdin@yandex.ru>
2018-08-25 22:26:13 +0400

    Добавил свою реализацию интерпретатора

--- behavior/interpreter.py
+++ behavior/interpreter.py
@@ -36,7 +36,23 @@ class RomanNumeralInterpreter(object):
             temp = num
         return result
 
+    def interpret2(self, text):
+        n = [self.grammar.get(s) for s in text]  # Вариант с map был "стройнее"
+        if None in n:
+            raise ValueError('Ошибочное значение: %s' % text)
+        result = 0  # Но сейчас обошлись без переменной temp...
+        for i in range(len(n)):  # ... и без изменения списка
+            if i == 0 or n[i - 1] >= n[i]:
+                result += n[i]
+            else:
+                result += (n[i] - n[i - 1] * 2)
+        return result
+
 
 interp = RomanNumeralInterpreter()
+print("Вариант 1")
 print interp.interpret('MMMCMXCIX') == 3999  # True
 print interp.interpret('MCMLXXXVIII') == 1988  # True
+print("Вариант 2")
+print interp.interpret2('MMMCMXCIX') == 3999  # True
+print interp.interpret2('MCMLXXXVIII') == 1988  # True
